6dfdddf59a8d055afbbd280c066bbdcd9ca52693
Park SungHyun <sunghyun0620@gmail.com>
2024-05-01 18:06:47 +0900

    회원가입 등록 완료

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    CATEGORY_CHOICES = (
+    ("M", "Male"),
+    ("F", "Female"),
+    )
+    first_name = None
+    last_name = None
+    email = models.EmailField(_("email address"),unique=True)
+    name=models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    birth=models.DateField()
+    sex= models.CharField(max_length=1, choices=CATEGORY_CHOICES, blank=True)
+    intro=models.TextField(blank=True)
+    
+    REQUIRED_FIELDS = ["email","name","nickname","birth",]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(["POST"])
+def signup(request):
+    serializer=UserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
